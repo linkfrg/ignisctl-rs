8d0675688de169a11487c07bbd2e344f6cbb7424
Link <linkfrg.dev@proton.me>
2025-07-10 00:43:00 +0500

    github(workflows): add ci.yaml

--- src/misc_utils.rs
+++ src/misc_utils.rs
@@ -4,7 +4,7 @@ pub fn resolve_path(raw_string: &String) -> String {
     let raw_path = Path::new(raw_string);
 
     match fs::canonicalize(raw_path) {
-        Ok(absolute_path) => return String::from(absolute_path.to_str().unwrap()),
+        Ok(absolute_path) => String::from(absolute_path.to_str().unwrap()),
         Err(e) => {
             eprintln!("Failed to resolve path: {e}");
             std::process::exit(1);
